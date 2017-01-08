28c6e39db912c43974d576506a02f0c641d92540
jorgecabreracruz <jorgecabreracruz@gmail.com>
2017-01-08 20:32:44 +0100

    Añadido test B

---
+++ src/tests/TestB.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestB {
+	
+	@Test
+	public void test_1(){
+		assertEquals(3,4-1);
+		assertEquals(3,resta(4,1);
+		
+
+	}
+	
+	
+	int resta (int a, int b){
+		return a-b;
+	}
+	
+}
