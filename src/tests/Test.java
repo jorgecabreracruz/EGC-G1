3bc59944eda1df0ee436397648ef2908cd8a0213
jorgecabreracruz <jorgecabreracruz@gmail.com>
2017-01-08 19:49:27 +0100

    Proyecto creado y test suma añadido

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -7,9 +7,24 @@ import org.junit.Test;
 public class Test {
 	
 	@Test
-	public void testSum2Number(){
+	public void test_1(){
 		assertEquals(5,4+1);
+		assertEquals(5,sum(4+1);
+		
+
+	}
+	
+	@Test
+	public void test_2(){
+		assertEquals(4,4+0);
+		assertEquals(4,sum(4+0);
+		
 
 	}
 
+	
+	int sum (int a, int b){
+		return a+b;
+	}
+	
 }
