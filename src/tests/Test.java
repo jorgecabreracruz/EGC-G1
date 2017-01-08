98959936dfb1ee5c1f5032314d6e2394f3b95df3
jorgecabreracruz <jorgecabreracruz@gmail.com>
2017-01-08 19:55:15 +0100

    Modificacion los dos tests y añadido la funcion suma

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -10,6 +10,7 @@ public class Test {
 	public void test_1(){
 		assertEquals(5,4+1);
 		assertEquals(5,sum(4+1);
+		assertEquals(5,FuncionSuma.sum(4+1);
 		
 
 	}
@@ -18,6 +19,7 @@ public class Test {
 	public void test_2(){
 		assertEquals(4,4+0);
 		assertEquals(4,sum(4+0);
+		assertEquals(4,FuncionSuma.sum(4+0);
 		
 
 	}
